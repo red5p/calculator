18dc0a36a51f106a94e0736e5a4a18c96c4d141e
Ryo <wh404@outlook.com>
2021-10-05 10:24:12 +0100

    Added basic layout and added event listener to number buttons to update the display

--- js/script.js
+++ js/script.js
@@ -13,3 +13,41 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(operator, a, b) {
+    switch (operator) {
+        case '+':
+            add(a, b); //? where should the return value go?
+            break;
+        case '-':
+            subtract(a, b);
+            break;
+        case '*':
+            multiply(a, b);
+            break;
+        case '/':
+            divide(a, b);
+    }
+}
+
+function updateCurrentInput(e) {
+    const input = e.target.textContent;
+    currentInput += input;
+}
+
+function updateDisplay() {
+    input.textContent = currentInput;
+}
+
+
+let currentInput = "";
+
+const input = document.querySelector("#input");
+
+const numberButtons = document.querySelectorAll("#buttons .number");
+numberButtons.forEach(button => {
+    button.onclick = (e) => {
+        updateCurrentInput(e);
+        updateDisplay();
+    }
+});
